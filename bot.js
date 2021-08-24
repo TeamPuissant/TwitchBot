0f864fb1dbbd76381d96acfd059ef44a96e8fd16
Derek <derekdinan@users.noreply.github.com>
2021-08-24 11:49:00 +1000

    Add leaderboard command

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
+const fs = require('fs')
 const TwitchBot = require('twitch-bot')
+
 require('dotenv').config()
 
 const Bot = new TwitchBot({
@@ -15,12 +17,21 @@ Bot.on('error', err => {
     console.log(err)
 })
 
-Bot.on('connect', channel => {
-    console.log('Logged in.')
-})
-
 Bot.on('message', chatter => {
-    if (chatter.message === '!test') {
-        Bot.say('Command executed! PogChamp')
+    if (chatter.message === '!leaderboard' || chatter.message === '!lb') {
+        const teams = []
+
+        const data = fs.readFileSync('C:/Users/User/Documents/GitHub/MCGalaxy/bin/Debug/plugins/Tournament/leaderboard.txt', 'UTF-8')
+        //const data = fs.readFileSync('../Plugins/Tournament/leaderboard.txt', 'UTF-8')
+
+        const lines = data.split(/\r?\n/)
+
+        lines.forEach((line) => {
+            teams.push(line)
+        })
+
+        Bot.say('🏆 Current leaderboard: 🥇 1. ' + teams[0] + ' 🥈 2. ' + teams[1] + ' 🥉 3. ' + teams[2] + ' 4. ' + teams[3] + ' 5. ' + teams[4] + ' 6. ' + teams[5] + ' 7. ' + teams[6] + ' 8. ' + teams[7])
     }
 })
+
+console.log('Logged in.')
